d2fc89b027b18cef7c30de3a8d102781a64b94cd
Leafmire <39892384+Leafmire@users.noreply.github.com>
2020-05-25 20:43:42 +0900

    #4.4 Styles Timelapse

--- src/Movie.js
+++ src/Movie.js
@@ -7,7 +7,7 @@ function Movie({ year, title, summary, poster, genres }) {
     <div className="movie">
       <img src={poster} alt={title} title={title}></img>
       <div className="movie__data">
-        <h3 className="movie__title" style={{ backgroundColor: "red" }}>
+        <h3 className="movie__title">
           {title}
         </h3>
         <h5 className="movie__year">{year}</h5>
